b6a34baf354c8549eb1d8842a3e7a7301264da04
Tekin Tian <tekintian@gmail.com>
2024-07-23 19:14:12 +0800

    泛型字符串到数字转换函数增加， go文件条件编译功能
    Signed-off-by: Tekin Tian <tekintian@gmail.com>

---
+++ convert_gt_go1.18.go
@@ -0,0 +1,24 @@
+//go:build go1.18
+// +build go1.18
+
+// 泛型的字符串相关转换函数
+// 这里仅支持go版本大于等于1.18的go运行环境,因为只有go版本大于等于1.18才支持泛型
+// @author: tekintian@gmail.com
+// @see https://dev.tekin.cn
+package strutils
+
+// 用于泛型约束的接口
+type iNumber interface {
+	int | int8 | int16 | int32 | int64 | float32 | float64 | uint | uint8 | uint16 | uint32 | uint64
+}
+
+// 泛型字符串转数字, 返回的类型为defVal对应的类型,如果转换失败,则返回默认值
+// 支持将字符串转换为go中所有的intx,uintx,floatx的数字类型
+func StrToNumber[T iNumber](str string, defVal T) T {
+	fval, err := strToFloat64(str)
+	if err != nil {
+		fval = float64(defVal)
+	}
+	// 强转为指定的T类型
+	return T(fval)
+}
---
+++ convert_gt_go1.18_test.go
@@ -0,0 +1,32 @@
+//go:build go1.18
+// +build go1.18
+
+// go 条件编译文件
+// 本文件仅在go运行环境版本大于等于 1.18时才会生效
+package strutils_test
+
+import (
+	"testing"
+
+	strutils "github.com/tekintian/go-str-utils"
+)
+
+func TestStrToNumber(t *testing.T) {
+	var intDefval int64 = 0 // 默认值,StrToNumber返回就就是这个默认值的类型
+	ival := strutils.StrToNumber("123.888abc", intDefval)
+	if ival != 123 {
+		t.Fatalf("Expected output to be 123, got %v", ival)
+	}
+
+	var intDefval2 int64 = 0 // 默认值,StrToNumber返回就就是这个默认值的类型
+	ival2 := strutils.StrToNumber("123,456.888abc", intDefval2)
+	if ival2 != 123456 {
+		t.Fatalf("Expected output to be 123456, got %v", ival)
+	}
+
+	var fDefval float64 = 0 // 默认值,StrToNumber返回就就是这个默认值的类型
+	fval := strutils.StrToNumber("hi123.888abc", fDefval)
+	if fval != 123.888 {
+		t.Fatalf("Expected output to be 123.888, got %v", ival)
+	}
+}
